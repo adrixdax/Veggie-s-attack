1735f671f5019990816cd7daec1933468aa9a510
MicheleZurlo01 <75538307+MicheleZurlo01@users.noreply.github.com>
2022-12-08 15:54:32 +0100

    x

--- Veggie's attack/GameViewController.swift
+++ Veggie's attack/GameViewController.swift
@@ -16,19 +16,22 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "Menu") {
+            if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
+                
+                /*
                 scene.scaleMode = .aspectFill
                 let play = SKShapeNode(rect: CGRect(x: -(UIScreen.main.bounds.width/3)/2, y: (UIScreen.main.bounds.height/4)/2, width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height/4), cornerRadius: 10);
                 play.fillColor = SKColor.systemBlue;
                 scene.addChild(play)
                 scene.addChild(SKShapeNode(rect: CGRect(x: -(UIScreen.main.bounds.width/3)/2, y: -(UIScreen.main.bounds.height/4)/2, width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height/4), cornerRadius: 10))
                 scene.addChild(SKShapeNode(rect: CGRect(x: -(UIScreen.main.bounds.width/3)/2, y: -(UIScreen.main.bounds.height/4)/(2/3), width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height/4), cornerRadius: 10))
+                 */
                 // Present the scene
                 view.presentScene(scene)
             }
             
-            view.ignoresSiblingOrder = true
+            view.ignoresSiblingOrder = false
             
             view.showsFPS = true
             view.showsNodeCount = true
