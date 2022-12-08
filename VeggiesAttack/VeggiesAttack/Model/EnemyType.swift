58068aba0093f3a848c2766dfd3d8e92b2155e01
MicheleZurlo01 <75538307+MicheleZurlo01@users.noreply.github.com>
2022-12-08 21:21:54 +0100

    new Project
    Arrivato al minuto 43 del video

---
+++ VeggiesAttack/VeggiesAttack/Model/EnemyType.swift
@@ -0,0 +1,17 @@
+//
+//  EnemyType.swift
+//  Test
+//
+//  Created by Michele Zurlo on 08/12/22.
+//
+
+import Foundation
+
+struct EnemyType: Codable{
+    
+    let name: String
+    let shields: Int
+    let speed: CGFloat
+    let powerUpChance: Int
+    
+}
---
+++ VeggiesAttack/VeggiesAttack/Model/Wave.swift
@@ -0,0 +1,21 @@
+//
+//  Wave.swift
+//  Test
+//
+//  Created by Michele Zurlo on 08/12/22.
+//
+
+import SpriteKit
+
+struct Wave: Codable{
+    
+    struct WaveEnemy: Codable{
+        
+        let position: Int
+        let xOffset: CGFloat
+        let moveStraight: Bool
+    }
+    
+    let name: String
+    let enemies: [WaveEnemy]
+}
