24ade1d96faddcad5b25f6797691731709f14966
MicheleZurlo01 <75538307+MicheleZurlo01@users.noreply.github.com>
2022-12-08 21:48:08 +0100

    aggiunto file per particles

--- VeggiesAttack/VeggiesAttack/GameScene.swift
+++ VeggiesAttack/VeggiesAttack/GameScene.swift
@@ -25,11 +25,23 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
         if let particles = SKEmitterNode(fileNamed: "MyParticle"){
+            
             particles.position = CGPoint(x: 1080,y: 0)
             particles.advanceSimulationTime(60)
             particles.zPosition = -1
+    
             addChild(particles)
         }
+        
+        /*
+         Other version to get particles file
+        let particlePath = Bundle.main.path(forResource: "MyParticle", ofType: "sks")
+        
+        let particle = NSKeyedUnarchiver.unarchiveObject(withFile: particlePath!) as! SKEmitterNode
+        
+        particle.position = CGPoint(x: 1080,y: 0)
+        addChild(particle)
+         */
         
         player.name = "playerSanto"
         player.position.x = frame.minX + 75
@@ -39,9 +51,7 @@ class GameScene: SKScene {
         player.physicsBody = SKPhysicsBody(texture: player.texture!, size: (player.texture!.size()))
         player.physicsBody?.categoryBitMask = CollisionType.player.rawValue
         player.physicsBody?.collisionBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
-        
         player.physicsBody?.contactTestBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
-        
         player.physicsBody?.isDynamic = false
     }
     
