b7cb30c03b23b6adf08b037998f4391c48072156
MicheleZurlo01 <75538307+MicheleZurlo01@users.noreply.github.com>
2022-12-08 18:55:04 +0100

    new project

---
+++ Veggie's Attack 2.0/Veggie's Attack 2.0/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Veggie's Attack 2.0
+//
+//  Created by Michele Zurlo on 08/12/22.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
