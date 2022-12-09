347ad6b06b23b3c54aa88f115935b5db8c5e3be8
Alessio Gazzara <81399617+ZodGaz@users.noreply.github.com>
2022-12-09 16:19:54 +0100

    Added enemy behaviour and update fuction
    Enemy now will spawn on screen, will fire projectiles based on their type and move using Bézier's curve ,also an update function will refresh the scenario sometimes

--- VeggiesAttack/VeggiesAttack/GameScene.swift
+++ VeggiesAttack/VeggiesAttack/GameScene.swift
@@ -21,8 +21,15 @@ class GameScene: SKScene {
     
     let waves = Bundle.main.decode([Wave].self, from: "waves.json")
     let enemyTypes = Bundle.main.decode([EnemyType].self, from: "enemy-types.json")
+    
+    var isPlayerAlive = true
+    var levelNumber = 0
+    var waveNumber = 0
+    
+    let positions = Array(stride(from: -320, through: 320, by: 80))
    
     override func didMove(to view: SKView) {
+        physicsWorld.gravity = .zero
         
         if let particles = SKEmitterNode(fileNamed: "MyParticle"){
             
@@ -48,12 +55,77 @@ class GameScene: SKScene {
         player.zPosition = 1
         addChild(player)
         
+        
+        // coding the physics 
         player.physicsBody = SKPhysicsBody(texture: player.texture!, size: (player.texture!.size()))
         player.physicsBody?.categoryBitMask = CollisionType.player.rawValue
         player.physicsBody?.collisionBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
         player.physicsBody?.contactTestBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
         player.physicsBody?.isDynamic = false
     }
+    
+    override func update(_ currentTime: TimeInterval){
+        for child in children {
+            if child.frame.maxX < 0 {
+                if !frame.intersects(child.frame) {
+                    child.removeFromParent()
+                }
+            }
+        }
+
+        let activeEnemies = children.compactMap { $0 as? EnemyNode }
+
+        if activeEnemies.isEmpty {
+            createWave()
+        }
+        
+        for enemy in activeEnemies {
+            guard frame.intersects(enemy.frame) else { continue }
+
+            if enemy.lastFireTime + 1 < currentTime {
+                enemy.lastFireTime = currentTime
+
+                if Int.random(in: 0...6) == 0 {
+                    enemy.fire()
+                }
+            }
+        }
+
+        
+    }
+    
+    
+
+    func createWave() {
+        guard isPlayerAlive else { return }
+
+        if waveNumber == waves.count {
+            levelNumber += 1
+            waveNumber = 0
+        }
+
+        let currentWave = waves[waveNumber]
+        waveNumber += 1
+
+        let maximumEnemyType = min(enemyTypes.count, levelNumber + 1)
+        let enemyType = Int.random(in: 0..<maximumEnemyType)
+
+        let enemyOffsetX: CGFloat = 100
+        let enemyStartX = 600
+
+        if currentWave.enemies.isEmpty {
+            for (index, position) in positions.shuffled().enumerated() {
+                let enemy = EnemyNode(type: enemyTypes[enemyType], startPosition: CGPoint(x: enemyStartX, y: position), xOffset: enemyOffsetX * CGFloat(index * 3), moveStraight: true)
+                addChild(enemy)
+            }
+        } else {
+            for enemy in currentWave.enemies {
+                let node = EnemyNode(type: enemyTypes[enemyType], startPosition: CGPoint(x: enemyStartX, y: positions[enemy.position]), xOffset: enemyOffsetX * enemy.xOffset, moveStraight: enemy.moveStraight)
+                addChild(node)
+            }
+        }
+    }
+    
     
 
     
